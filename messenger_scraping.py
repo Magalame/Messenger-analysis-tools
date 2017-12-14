b9477214e6d22e076662755062098e3678a5b52c
LawLawL <nbaspin@gmail.com>
2017-12-13 23:35:05 -0400

    wrapped getMessageList into scrapeMessages

--- messenger_scraping.py
+++ messenger_scraping.py
@@ -3,16 +3,16 @@
 import argparse
 from fbchat import log, Client
 from fbchat.models import *
+import getpass
+import os
+import sys
 
 parser = argparse.ArgumentParser()
 
 parser.add_argument('-p','--password', action="store", dest="password", help="Your facebook password")
 parser.add_argument('-a','--email_address', action="store", dest="address", help="Your email address")
 
-
-client = Client(args.address, args.address)
-
-
+args = parser.parse_args()
 
 def appendItems(source, target): #appends the elements of a list to another one
     for i in source:
@@ -38,7 +38,7 @@ def printFriends(client): #prints friend list with IDs
         if user.uid != "0" and user.uid != 0:
             print(user.name,a*"\t",user.uid)
 
-
+#checks that we haven't double included a message in a list
 def check_for_duplication(liste): #https://stackoverflow.com/questions/1541797/check-for-duplicates-in-a-flat-list
   seen = set()
   for i,x in enumerate(liste):
@@ -49,16 +49,18 @@ def check_for_duplication(liste): #https://stackoverflow.com/questions/1541797/c
   return False
 
 def printMsg(liste, personnes): #print the messeges in a list of message objects
-    #personnes = {}#dictionnary with the persons in the chat
+    personnes = {}#dictionnary with the persons in the chat
     for i in liste:
         print(personnes[i.author] + ": " + i.text)
 
 
 #if scraping for a one-to-one chat then set thread_type = ThreadType.USER
 # for groups use ThreadType.GROUP
-#client should be the object created with "client = Client(args.address, args.address)"
+#client should be the Client object we just created
 #and "thread_id" the ID of the thread you're interested in
 def getMessageList(client, thread_id, thread_type):
+    
+    print("Scraping the message list...")
 
     msg_list = []
     count = 1
@@ -68,6 +70,7 @@ def getMessageList(client, thread_id, thread_type):
     appendItems(messages, msg_list)
 
     while True:
+        print(str(count*10000) + "th message loaded")
         messages = client.fetchThreadMessages(thread_id=thread_id, limit=10000, before=messages[-1].timestamp)
         appendItems(messages[1:], msg_list)  #we remove the first one because of the way the fetchThread function works. We could use "before=messages[-1].timestamp-1" and keep the first element for clarity's sake but it works as well without
         count = count + 1
@@ -75,5 +78,49 @@ def getMessageList(client, thread_id, thread_type):
     
         if messages[0].uid == messages[-1].uid:#as we remove the last one, we do not double count the last list (which will have only one element)
             break
-
+    print("All messages are loaded")
+    
     return msg_list
+
+def scrapeMessages(address='', password='', thread_id='', is_group=False):
+    
+    while not address:
+        address = input("Please enter your email adress:")
+    
+    while not password:
+        if any('SPYDER' in name for name in os.environ) or "pythonw.exe" in sys.executable:
+            password = input("Please enter your password: ")
+        else:
+            password = getpass.getpass("Please enter your password: ")
+            
+    client = Client(address, password)
+            
+    if not thread_id:
+        choice = ""
+        
+        while choice.lower() != 'g' and choice.lower() != 'u':
+            choice = input("Do you want to scrape a group chat (\'g\') or a simple chat (\'u\')")
+        if choice == 'u':
+            is_group = False
+            while choice.lower() != "y" and choice.lower() != "n":
+                choice = input("Do you want to print your friends list, with their ID? (Press \'y\' if you're not sure) [y/n]:")
+    
+            if choice.lower() == "y":
+                printFriends(client)
+        
+        if choice == 'g':
+            is_group = True
+            
+    while not thread_id:    
+        thread_id = input("Please enter the ID: ")
+        
+    if is_group:#we keep it outside of the "if not thread_id:" condition because we also want it to work for parameters
+        thread_type = ThreadType.GROUP
+    else:
+        thread_type = ThreadType.USER
+        
+    return getMessageList(client, thread_id, thread_type)
+
+if __name__ == "__main__":
+    if args.address and args.password:
+        scrapeMessages(args.address, args.password)
