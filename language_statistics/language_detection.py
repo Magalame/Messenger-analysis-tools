222ecb9deacf9c18631b4223836b63230ea99491
a-coles <arlie.coles@mail.mcgill.ca>
2017-12-19 20:57:35 -0600

    Language detection export

--- language_statistics/language_detection.py
+++ language_statistics/language_detection.py
@@ -1,6 +1,7 @@
 import json
 import time
 import datetime
+import csv
 import sys
 sys.path.append('..')
 
@@ -8,20 +9,32 @@ from tools import preprocessing
 from langdetect import detect
 
 messages_path = sys.argv[1]
+export_path = sys.argv[2]
 
-def fill_language(messages):
+def fill_language(messages, languages=None):
     # Adds a language field to the message dictionary
+    filled_messages = []
     for key, value in messages.items():
-        if key > 10:
-            break
+        #if key > 10:   # Debugging
+        #    break
         try:
             lang = detect(value['Text'])
         except:
             lang = "??"
+        if languages != None and lang not in languages:
+            lang = "other"
         value['lang'] = lang
-    return messages
+        filled_messages.append(value)
+    return filled_messages
+
+def export_lang_csv(filled_messages, export_path):
+    with open(export_path, 'w') as fp:
+        w = csv.DictWriter(fp, filled_messages[0].keys())
+        w.writeheader()
+        w.writerows(filled_messages)
 
 def lang_by_author_by_time(messages, languages=None, all_time=True, beg=None, end=None):
+    # Get language for each message over a range of dates (format YYYY-MM-DD)
     by_author = {}
     if all_time == True:
         beg = messages[1]['Date']
@@ -56,7 +69,6 @@ def lang_by_author_by_time(messages, languages=None, all_time=True, beg=None, en
         else:
             by_author[author_name][lang] += 1
 
-
     print(by_author)
     return by_author
 
@@ -65,9 +77,6 @@ if __name__ == '__main__':
     messages = preprocessing.load_messages(messages_path)
 
     # Fill with language
-    by_author = lang_by_author_by_time(messages, languages=['en', 'fr'], beg='2017-5-12', end='2017-5-20')
-
-    """for key, value in messages.items():
-        print(key)
-        print(value)
-        break"""
+    #by_author = lang_by_author_by_time(messages, languages=['en', 'fr'], beg='2017-5-12', end='2017-5-20')
+    filled_messages = fill_language(messages, languages=['en', 'fr'])
+    export_lang_csv(filled_messages, export_path)
