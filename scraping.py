749d13f1606b52a331756df5d4c3c2a0793431c7
LawLawL <nbaspin@gmail.com>
2017-12-20 05:15:10 -0400

    added upper and lower time bound, count bound in scraping function

--- scraping.py
+++ scraping.py
@@ -43,90 +43,159 @@ def printFriends(client): #prints friend list with IDs
 #checks that we haven't double-included a message in a list
 #maybe replace "true" by an exception
 def check_for_duplication(liste): #https://stackoverflow.com/questions/1541797/check-for-duplicates-in-a-flat-list
+    
   seen = set()
+  
   for i,msg in enumerate(liste):
+      
     if msg.uid in seen: 
+        
         print("Duplication at " + str(i))
+        
         return True
+    
     seen.add(msg.uid)
+    
   return False
 
 #checks that there is no time discontinuity in the list
 def check_for_time(liste): 
+    
     for i in range(1,len(liste)):
+        
         if int(liste[i].timestamp) > int(liste[i-1].timestamp): #assuming that the lastest message is at index 0
+            
             print("Fails at index " + str(i))
+            
             return False
+        
     return True
 
-def get_attachment_index(liste): #returns a list with the indexes of all objects having an attachment
+def get_attachment_indexes(liste): #returns a list with the indexes of all objects having an attachment
+    
     attached = []
+    
     for i,msg in enumerate(liste):
+        
         if msg.attachments != [] and msg.attachments != None:
-            #print("Attachment at index " + str(i))
+            
             attached = attached + [i]
+            
     return attached
 
 #returns a dictionnary classifying the indexes of the list according to what kind of attachment do they have
 def classify_attachments(liste):
-    indexes = get_attachment_index(liste)
+    
+    indexes = get_attachment_indexes(liste)
     classified = {}
+    
     for i in indexes:
+        
         for ii,attachment in enumerate(liste[i].attachments):
+            
             if attachment['__typename'] not in classified.keys():
+                
                 classified[attachment['__typename']] = [(i,ii)]#we put two indexes, i and ii in the dictionnary, because i corresponds to the position in the list, and ii to the position of the attachment in the message
+
             else:
+                
                 classified[attachment['__typename']].append((i,ii))
+                
     return classified
 
 def write_datetime_from_timestamp(liste, utc = False):
+    
     if not utc:
+        
         for msg in liste:
+            
             msg.datetime = datetime.datetime.fromtimestamp(int(msg.timestamp[:-3])) #we stripe the last 3 digits because python doesn't handle it
+
     else:
+        
         for msg in liste:
+            
             msg.datetime = datetime.datetime.utcfromtimestamp(int(msg.timestamp[:-3]))
             
 def get_name_from_id(client,ID):
    return client.fetchUserInfo(ID)[ID].name
 
 def write_name_from_id(client, liste):
+    
     id_to_name = {}
+    
     for i in liste:
         if i.author not in id_to_name.keys(): #a bit of dynamic programming, otherwise it takes ages
+
             id_to_name[i.author] = get_name_from_id(client,i.author)
+
         i.author_name = id_to_name[i.author]
+
     return id_to_name #originally, it isn't the purpose of the function to return the dictionnary, but why waste it? 
+
+def remove_timestamp_overhead(liste,timestamp):
+    
+    print('Removing timestamp overhead...')
+    
+    while int(liste[-1].timestamp) < timestamp:
+        
+        liste.pop()
+        
+def remove_counter_overhead(liste,upper_bound):
+    
+    print('Removing counter overhead...')
+    
+    while len(liste) > upper_bound:
+        
+        liste.pop()
 
 #client should be the Client object we just created
 #and "thread_id" the ID of the thread you're interested in
-def getMessageList(client, thread_id, verbose=1):
+#messages_before should be a int representing a unix timestamp
+def getMessageList(client, thread_id, verbose=1, messages_before=-1, messages_after=-1, upper_bound=-1):
     
     print("Scraping the message list...")
 
-    #msg_list = []
     count = 1
-    
-    cur_time = start_time = time.time()
     cur_size = 0
-    messages = client.fetchThreadMessages(thread_id=thread_id, limit=10000)
+    if messages_before < 0:
+        messages = client.fetchThreadMessages(thread_id=thread_id, limit=10000)
+    else:
+        messages = client.fetchThreadMessages(thread_id=thread_id, limit=10000, before=messages_before)
     msg_list = messages
+    cur_time = start_time = time.time()
 
+    print("At most 10000 messages loaded")
+    
     while True:
         
-        if verbose == 2:
-            print(str(count*10000) + "th message in " + str(time.time() - start_time)[:-13] + "s" + "\t| " + str(sys.getsizeof(msg_list)) + " B" + "\t increase: " + str(sys.getsizeof(msg_list)-cur_size) + "\t length: " + str(len(msg_list)) + "\t delay: " + str(time.time()-cur_time)[:-13] + "s")
-            cur_time = time.time()
-            cur_size = sys.getsizeof(msg_list)
-        elif verbose == 1:
-            print(str(count*10000) + "th message loaded")
+        if messages_after >= 0:
+            if int(messages[0].timestamp) < messages_after or int(messages[-1].timestamp) < messages_after:
+                remove_timestamp_overhead(msg_list,messages_after)
+                break
+            
+        if upper_bound >= 0:
+            if len(msg_list) > upper_bound:
+                remove_counter_overhead(msg_list,upper_bound)
+                break
+            elif len(msg_list) == upper_bound:
+                break
         
         messages = client.fetchThreadMessages(thread_id=thread_id, limit=10000, before=messages[-1].timestamp)
-
+        
         msg_list = msg_list + messages[1:]  #we remove the first one because of the way the fetchThread function works. We could use "before=int(messages[-1].timestamp)-1" and keep the first element for clarity's sake but it works as well without
 
         count = count + 1
-
+        
+        if verbose == 2:
+            
+            print("At most " + str(count*10000) + " messages in " + str(time.time() - start_time)[:-13] + "s" + "\t| " + str(sys.getsizeof(msg_list)) + " B" + "\t increase: " + str(sys.getsizeof(msg_list)-cur_size) + "\t length: " + str(len(msg_list)) + "\t delay: " + str(time.time()-cur_time)[:-13] + "s")
+            cur_time = time.time()
+            cur_size = sys.getsizeof(msg_list)
+            
+        elif verbose == 1:
+            
+            print("At most",str(count*10000),"messages loaded")
     
         if messages[0].uid == messages[-1].uid:#as we remove the first one, we do not double count the last list (which will have only one element)
             break
@@ -203,7 +272,7 @@ def save_msg_json(liste, namefile):
             json.dump(i.__dict__, f,indent=4, separators=(',', ': '))
             f.write(',')
             
-        for i in handle_message(liste[-1:]):
+        for i in handle_message(liste[-1:]): #we put the last element in each own loop too be able to control for the last comma to write
             json.dump(i.__dict__, f,indent=4, separators=(',', ': '))
             
         f.write(']')
@@ -235,9 +304,9 @@ def create_lambda_values_msg(fieldnames):
     fieldnames_checked = []
     
     for field in fieldnames:
-        if field == "Date":
-            lambda_values.append(lambda msg:msg.datetime)
-            fieldnames_checked.append("Date")
+        if field == "Datetime":
+            lambda_values.append(lambda msg:str(msg.datetime))
+            fieldnames_checked.append("Datetime")
         elif field == "Author":
             lambda_values.append(lambda msg:msg.author)
             fieldnames_checked.append("Author")
@@ -256,13 +325,16 @@ def create_lambda_values_msg(fieldnames):
             
     return lambda_values, fieldnames_checked
             
-def save_msg_csv(liste, namefile, values_to_save):
+def save_msg_csv(liste, namefile, values_to_save = ["Datetime","AuthorName","Text","MessageID"]):
+    
     with open(namefile, "w", newline='',encoding='utf-8') as pfile:
+        
         csv_writer = csv.writer(pfile)
         
         lambda_values, values_to_save_checked = create_lambda_values_msg(values_to_save)
-        #test = lambda _in: csv_writer.writerow([_in.datetime,_in.author,_in.text,_in.uid])
+
         csv_writer.writerow(values_to_save_checked)
+        
         for msg in liste[::-1]:
             if msg.text != '' and msg.text != None:
                 #test(i)
